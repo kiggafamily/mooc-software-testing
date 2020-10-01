1c86bbc00392a24ab57520c45c6f0ba376d68c4d
kiggafamily <kiggafamily@gmail.com>
2020-10-01 15:48:27 +0800

    Indentified Bug and Fixed.

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,24 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    @ParameterizedTest
+    @CsvSource ({ "xxggxx", "xxgxx", "xxggyygxx", "g" , "gg", "xxgggxyz","xxgggxyg"})
+    public void happy(String ghStr) {
+        GHappy gHpy = new GHappy();
+        boolean result = gHpy.gHappy(ghStr);
+        if ( result ) {
+            Assertions.assertTrue(result);
+        }
+        else {
+            Assertions.assertFalse(result);
+        }
+
+    }
+
+
 }
