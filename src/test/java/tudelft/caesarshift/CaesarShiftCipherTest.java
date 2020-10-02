8776b00991c93bc67bdc2d38c03a095ab190eb34
kiggafamily <kiggafamily@gmail.com>
2020-10-02 13:40:53 +0800

    Completed the test.

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,21 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import tudelft.ghappy.GHappy;
+
 public class CaesarShiftCipherTest {
+
+    @ParameterizedTest
+    @CsvSource({ "xyz, 3", "deef, 2", "abcd, 4"})
+    public void cShiftCiper(String shiftMessage, int shift) {
+        CaesarShiftCipher cShift = new CaesarShiftCipher();
+        String result = cShift.CaesarShiftCipher(shiftMessage, shift);
+
+        Assertions.assertNotNull(result);
+
+
+    }
 
 }
